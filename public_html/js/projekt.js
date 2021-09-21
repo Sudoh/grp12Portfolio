fb142a797d48248f5e5078d4009811ce80286148
Payam K <payam@karimian.se>
2021-09-21 19:17:40 +0200

    Update projekt.js

--- public_html/js/projekt.js
+++ public_html/js/projekt.js
@@ -5,11 +5,17 @@
         dataType: "xml",
         success: function (xml) {
             console.log($(xml));
-            $(xml).find("projekt").each(function(index, projekt) {
+            $(xml).find("projekt").each(function() {
                 
-
-                $('#projekt').append('<div>' + $(this).find("namn").text() + '</div>');
-               
+                //Ritar upp tabell med hjälp av Divs
+                $('#projekt').append('<div id="projektNamn"><h1 id="projektNamn">' + $(this).find("namn").text() + '</h1></div>');
+                $('#projekt').append('<div id="projektLedare"><p>' + $(this).find("projektLedare").text() + '</p></div>');
+                $('#projekt').append('<div id="uppdragsgivare">' + $(this).find("uppdragsgivare").text() + '</p></div>');
+                $('#projekt').append('<div id="startDatum">' + $(this).find("startDatum").text() + '</p></div>');
+                $('#projekt').append('<div id="slutDatum">' + $(this).find("slutDatum").text() + '</p></div>');
+                $('#projekt').append('<div id="projektBild"><img src="/bilder/projekt/' + $(this).find("bildURL").text() + '" alt="' + $(this).find("namn").text() + '"/></div>');
+                $('#projekt').append('<div id="projektBeskrivning">' + $(this).find("projektBeskrivning").text() + '</div>');
+                
 
             })
 
