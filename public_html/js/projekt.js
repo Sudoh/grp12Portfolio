ba676bd1e946d9118d7a5cc2a869b978664e8926
Payam K <payam@karimian.se>
2021-09-22 14:22:50 +0200

    projekt

--- public_html/js/projekt.js
+++ public_html/js/projekt.js
@@ -6,16 +6,16 @@
         success: function (xml) {
             
             $(xml).find("projekt").each(function() {
-                
+
                 //Ritar upp tabell med hjälp av Divs
                 $('#projekt').append('<div class="projektInfo">' + 
                 '<div class="projektBild"><img src="/bilder/projekt/' + $(this).find("bildURL").text() + '" alt="' + $(this).find("namn").text() + '"/></div>' + 
                 '<div class="projektNamn"><h1 class="projektNamn">' + $(this).find("namn").text() + '</h1></div>' + 
-                '<div class="projektLedare"><p><span class="pHeading">Projektledare: </span class="pHeading">' + $(this).find("projektLedare").text() + '</p></div>' + 
-                '<div class="uppdragsgivare"><p><span class="pHeading">Uppdragsgivare: </span class="pHeading">' + $(this).find("uppdragsgivare").text() + '</p></div>' + 
-                '<div class="startDatum"><p><span class="pHeading">Startdatum: </span class="pHeading">' + $(this).find("startDatum").text() + '</p></div>' + 
-                '<div class="slutDatum"><p><span class="pHeading">Slutdatum: </span class="pHeading">' + $(this).find("slutDatum").text() + '</p></div>' +
-                '<div class="projektBeskrivning"><p><span class="pHeading">Projektbeskrivning: </span class="pHeading">' + $(this).find("projektBeskrivning").text() + '</p></div>' + 
+                '<div class="projektLedare"><p><span class="pHeading">Projektledare: </span>' + $(this).find("projektLedare").text() + '</p></div>' + 
+                '<div class="uppdragsgivare"><p><span class="pHeading">Uppdragsgivare: </span>' + $(this).find("uppdragsgivare").text() + '</p></div>' + 
+                '<div class="startDatum"><p><span class="pHeading">Startdatum: </span>' + $(this).find("startDatum").text() + '</p></div>' + 
+                '<div class="slutDatum"><p><span class="pHeading">Slutdatum: </span>' + $(this).find("slutDatum").text() + '</p></div>' +
+                '<div class="projektBeskrivning"><p><span class="pHeading">Projektbeskrivning: </span>' + $(this).find("projektBeskrivning").text() + '</p></div>' + 
                 '</div>');
             })
 
