0075dc06f10d10f18e11388e1f6614d0ef768628
Amira-sawas <91065425+Amira-sawas@users.noreply.github.com>
2021-09-27 22:24:20 +0200

    lll
    lll

--- public_html/js/Amira.js
+++ public_html/js/Amira.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   // Apearing tha paragraph
-  $("#app").fadeToggle(5000);
+  $("#app-a").fadeToggle(5000);
 
   // // Select page
-  let portfolioSida = document.querySelector(".portfolio-sida");
+  let portfolioSida = document.querySelector(".portfolio-sida-a");
 
   // Get Array of images;
   let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "05.jpg", "06.jpg"];
@@ -16,4 +16,8 @@ $(document).ready(function () {
     portfolioSida.style.backgroundImage =
       'url("../bilder/index-images' + imgsArray[randomNumber] + '")';
   }, 10000);
+
+  $("#btn-a").click(function () {
+    $(".links-a").slideToggle(1000);
+  });
 });
