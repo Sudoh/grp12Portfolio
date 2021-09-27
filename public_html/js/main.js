75bfa063fe535d9b794279d2c2085ef119328179
eddero <edvin97johansson@hotmail.com>
2021-09-27 15:14:37 +0200

    Nav Bar
    test1

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,9 @@
 /* Grupp 12 JavaScript */
+
+/*Js for toggle-nav */
+const toggleButton = document.getElementsByClassName('nav__toggle')[0]
+const navbarLinks = document.getElementsByClassName('nav__list')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('show')
+})
