1ace90798d62eb34db05f5bf739a6e1efb35a3c2
eddero <edvin97johansson@hotmail.com>
2021-09-28 11:44:03 +0200

    fullscreen
    api

--- public_html/js/appEd.js
+++ public_html/js/appEd.js
@@ -95,3 +95,18 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();    
   }
 });
+
+/*Js for Full-Screen */
+
+function toggleFullscreen(){
+  if (getFullscreemElement()) {
+    document.exitFullscreen();
+  }else {
+    document.documentElement.requestFullscreen.catch(e);
+  }
+}
+document.addEventListener("dblclick", () => {
+  document.documentElement.requestFullscreen().catch((e) =>{
+
+  })
+})
