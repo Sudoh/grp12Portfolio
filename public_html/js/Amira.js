772c98924a8f384debd873cf84e05e6d4058adf6
Amira-sawas <91065425+Amira-sawas@users.noreply.github.com>
2021-09-28 09:35:05 +0200

    sss
    sss

--- public_html/js/Amira.js
+++ public_html/js/Amira.js
@@ -5,17 +5,17 @@ $(document).ready(function () {
   // // Select page
   let portfolioSida = document.querySelector(".portfolio-sida-a");
 
-  //   // Get Array of images;
-  //   let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "05.jpg", "06.jpg"];
+    // Get Array of images;
+    let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "05.jpg", "06.jpg"];
 
-  //   setInterval(function () {
-  //     // Get random Number
-  //     let randomNumber = Math.floor(Math.random() * imgsArray.length);
+    setInterval(function () {
+      // Get random Number
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
 
-  //     // Change Background Image Ur1
-  //     portfolioSida.style.backgroundImage =
-  //       'url("../bilder/index-images' + imgsArray[randomNumber] + '")';
-  //   }, 10000);
+      // Change Background Image Ur1
+      portfolioSida.style.backgroundImage =
+        'url("bilder/index-images/' + imgsArray[randomNumber] + '")';
+    }, 10000);
 
   $("#btn-a").click(function () {
     $(".links-a").slideToggle(1000);
