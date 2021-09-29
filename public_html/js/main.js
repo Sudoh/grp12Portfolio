529373b396bba72bf08222ea3070552621ab9b1f
Payam K <payam@karimian.se>
2021-09-29 13:00:14 +0200

    Update main.js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -8,57 +8,71 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('show')
 });
 
-    
-    
-    //Laddar med start.html på en gång så att det inte blir en tom sida
-    $('#container').load('start.html' + ' #content').hide().fadeIn(500);
-    $('#content').append($('<script src=js/Amira.js />'));
+// SCRIPT FÖR FULLSCREEN 
 
-    $('nav a').on('click', function (e) {
-      e.preventDefault();
-      var id = this.id;
-      var url = this.href;
+function toggleFullscreen(){
+  if (getFullscreemElement()) {
+    document.exitFullscreen();
+  }else {
+    document.documentElement.requestFullscreen.catch(e);
+  }
+};
+document.addEventListener("dblclick", () => {
+  document.documentElement.requestFullscreen().catch((e) =>{
 
-      console.log(this.href);
-
-      $('nav a.current').removeClass('current');
-      $(this).addClass('current');
-
-      $('#content').hide();
-      $('#container').load(url + ' #content').hide().fadeIn(500);
-
-      switch (id) {
-
-        case 'startLink':
+  });
+});
 
 
-          $('#content').append($('<script src=js/Amira.js />'));
-          console.log("lägger till amira js");
+//Laddar med start.html på en gång så att det inte blir en tom sida
+$('#container').load('start.html' + ' #content').hide().fadeIn(500);
+$('#content').append($('<script src=js/Amira.js />'));
 
-          break;
+$('nav a').on('click', function (e) {
+  e.preventDefault();
+  var id = this.id;
+  var url = this.href;
 
-        case 'projektLink':
+  console.log(this.href);
 
-          console.log("lägger till projekt js");
-          $('#content').append($('<script src=js/projekt.js />'));
+  $('nav a.current').removeClass('current');
+  $(this).addClass('current');
+
+  $('#content').hide();
+  $('#container').load(url + ' #content').hide().fadeIn(500);
+
+  switch (id) {
+
+    case 'startLink':
 
 
-          break;
+      $('#content').append($('<script src=js/Amira.js />'));
+      console.log("lägger till amira js");
 
-        case 'omOssLink':
+      break;
 
-          break;
+    case 'projektLink':
 
-        case 'kontaktLink':
-        
-          $('#content').append($('<script src=js/appEd.js />'));
-          $('#content').append($('<script src=js/Amira.js />'));
+      console.log("lägger till projekt js");
+      $('#content').append($('<script src=js/projekt.js />'));
 
-          break;
 
-        default:
-          console.log('Något gått fel, kontrollera variabeler och id');
+      break;
 
-      }
+    case 'omOssLink':
 
-    });
+      break;
+
+    case 'kontaktLink':
+
+      $('#content').append($('<script src=js/appEd.js />'));
+      $('#content').append($('<script src=js/Amira.js />'));
+
+      break;
+
+    default:
+      console.log('Något gått fel, kontrollera variabeler och id');
+
+  }
+
+});
