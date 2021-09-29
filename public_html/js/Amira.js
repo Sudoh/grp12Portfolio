a689c2b89c726c94e3806ce06a1ebb81d0c8a910
Amira-sawas <91065425+Amira-sawas@users.noreply.github.com>
2021-09-29 15:15:42 +0200

    fff
    fff

--- public_html/js/Amira.js
+++ public_html/js/Amira.js
@@ -1,22 +1,52 @@
 $(document).ready(function () {
-
   // Apearing tha paragraph
-  $("#app").fadeIn(1000);
+  $("#p-toggle").fadeIn(10000);
 
   // // Select page
   let portfolioSida = document.querySelector(".portfolio-sida-a");
 
+  //Random Background Option
+  let backgroundOption = true;
+
+  //Variable to Control The Interval
+  let backgroundInterval;
+
+  //Swiitch Random Background Option
+  const randomBackground = document.querySelectorAll(
+    ".random-background button"
+  );
+
+  //Loop On All Buttons
+  randomBackground.forEach(buttn => {
+    //Click On All Buttons
+    buttn.addEventListener("click", e => {
+      if (e.target.dataset.background === "yes") {
+        backgroundOption = true;
+
+        randomizeImgs();
+      } else {
+        backgroundOption = false;
+
+        clearInterval(backgroundInterval);
+      }
+    });
+  });
+
   // Get Array of images;
   let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "05.jpg", "06.jpg"];
 
-  setInterval(function () {
-    // Get random Number
-    let randomNumber = Math.floor(Math.random() * imgsArray.length);
+  // function To Randomize Images
+  function randomizeImgs() {
+    if (backgroundOption === true) {
+      setInterval(function () {
+        // Get random Number
+        let randomNumber = Math.floor(Math.random() * imgsArray.length);
 
-    // Change Background Image Ur1
-    portfolioSida.style.backgroundImage = 
-      'url("bilder/index-images/' + imgsArray[randomNumber] + '")';
-  }, 2500);
-
-
+        // Change Background Image Ur1
+        portfolioSida.style.backgroundImage =
+          'url("bilder/index-images/' + imgsArray[randomNumber] + '")';
+      }, 2500);
+    }
+  }
+  randomizeImgs();
 });
