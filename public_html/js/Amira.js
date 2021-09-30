e84f5ecf56318ed468f461e2db1a4d6af480fd02
Payam K <payam@karimian.se>
2021-09-30 11:47:21 +0200

    Final

--- public_html/js/Amira.js
+++ public_html/js/Amira.js
@@ -1,50 +1,60 @@
-/* $(document).ready(function () { */
-// Apearing tha paragraph
-$("#p-toggle").fadeIn(10000);
+$(document).ready(function () {
+  // Apearing tha paragraph
+  $("#p-toggle").fadeIn(10000);
 
-// // Select page
-let portfolioSida = document.querySelector(".portfolio-sida-a");
-const startButton = document.getElementsByClassName('animationAction')[0];
-const stopButton = document.getElementsByClassName('animationPause')[0];
+  // Fångar element från sidan och deklarerar som variabel
+  let portfolioSida = document.querySelector(".portfolio-sida-a");
+  const startButton = document.getElementsByClassName('animationAction')[0];
+  const stopButton = document.getElementsByClassName('animationPause')[0];
 
-//Switch Random Background Option
-const randomBackground = document.querySelectorAll(
-  ".random-background button"
-);
+  //Switch Random Background Option
+  const randomBackground = document.querySelectorAll(
+    ".random-background button"
+  );
+
+  // Deklarerar variabler
+  let intervall;
+  // Get Array of images;
+  let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "05.jpg", "06.jpg"];
 
 
-let intervall
-// Get Array of images;
-let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "05.jpg", "06.jpg"];
-
-// function To Randomize Images
-function randomizeImgs() {
-
-  intervall = setInterval(function () {
-    // Get random Number
-    let randomNumber = Math.floor(Math.random() * imgsArray.length);
-
-    // Change Background Image Ur1
-    portfolioSida.style.backgroundImage =
-      'url("bilder/index-images/' + imgsArray[randomNumber] + '")';
-  }, 1000);
-  startButton.disabled = true; 
-  stopButton.disabled = false; 
-
-}
-
-startButton.addEventListener('click', () => {
+  // Startar animationen när sidan laddas
+  startButton.disabled = true;
   randomizeImgs();
-})
 
-stopButton.addEventListener('click', () => {
-  myStopFunction();
-})
 
-function myStopFunction() {
-  clearTimeout(intervall);
-  startButton.disabled = false; 
-  stopButton.disabled = true; 
-}
+  // function To Randomize Images
+  function randomizeImgs() {
 
-/* }); */
+    intervall = setInterval(function () {
+      // Get random Number
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
+
+      // Change Background Image Ur1
+      portfolioSida.style.backgroundImage =
+        'url("bilder/index-images/' + imgsArray[randomNumber] + '")';
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+  }
+
+
+  // fångar knapparna och kör funktion beroende av knapp
+  startButton.addEventListener('click', () => {
+    randomizeImgs();
+  })
+
+  stopButton.addEventListener('click', () => {
+    myStopFunction();
+  })
+
+
+  // Funktion som disablar start/stopp knappar
+  function myStopFunction() {
+    clearTimeout(intervall);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
+
+});
